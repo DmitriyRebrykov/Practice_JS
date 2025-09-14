a198142789be2445c12046a975ece21f42b2ae67
CRON997 <dmitriy.rebrikov2006@gmail.com>
2025-09-14 23:47:05 +0300

    Ended lesson 2

--- L2/app.js
+++ L2/app.js
@@ -13,15 +13,23 @@ const inputElement = document.getElementById('title');
 const createBtn = document.getElementById('create');
 const listElement = document.getElementById('list');
 
-// console.log(inputElement.value)
 
-const notes = ['Write block about array', 'Something']
+const notes = [
+{
+  title:'Write block about array',
+  completed: false,
+}, 
+{
+  title:'Something',
+  completed: true,
+}, 
+]
 
 function render(){
-  
+  listElement.innerHTML = ''
   for(let i=0; i < notes.length; i++) 
   {
-    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i]))
+    listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
   }
 }
 render()
@@ -30,33 +38,62 @@ createBtn.onclick = function (){
     if (inputElement.value.length === 0){
         return
     }
-    // listElement.innerHTML = 
-    listElement.insertAdjacentHTML('beforeend',getNoteTemplate(inputElement.value))
+    const newNote = {
+      title:inputElement.value,
+      completed: false,
+    }
+    notes.push(newNote)
+    render()
     inputElement.value = ''
 }
 
-function getNoteTemplate(title){
-  return `<li class="list-group-item d-flex justify-content-between align-items-center">
-          <span>${title}</span>
-          <span>
-            <button class="btn btn-sm btn-success me-1">&check;</button>
-            <button class="btn btn-sm btn-danger">&times;</button>
-          </span>
-        </li>`
+listElement.onclick = function (event){
+  if(event.target.dataset.index)
+  {
+    const index = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+
+    if (type == 'toggle')
+    {
+      notes[index].completed = !notes[index].completed
+    } 
+    else if (type == 'remove')
+      {
+        notes.splice(index, 1)
+      }
+      render()
+  }
 }
+
+function getNoteTemplate(note, index) {
+  return `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+      <span class="${note.completed ? 'text-decoration-line-through' : ''}">${note.title}</span>
+      <span>
+        <button class="btn btn-sm btn-${note.completed ? 'warning' : 'success'} me-1" data-index="${index}" data-type="toggle">&check;</button>
+        <button class="btn btn-sm btn-danger" data-index="${index}" data-type="remove">&times;</button>
+      </span>
+    </li>
+  `
+}
+
 
 /*
 Type object
 */ 
 
-const person = {
-  firstName:'Dmytro',
-  lastName:'Rebrykov',
-  yearOfBirth:2006,
-  hasGirlfriend:false,
-  languages:['ru','uk','en'],
-  getFullName: function(){
-    co
-  }
-}
-console.log(person)
+// const person = {
+//   firstName:'Dmytro',
+//   lastName:'Rebrykov',
+//   yearOfBirth:2006,
+//   hasGirlfriend:false,
+//   languages:['ru','uk','en'],
+//   getFullName: function(){
+//     console.log(person.firstName + ' ' + person.lastName)
+//   }
+// }
+
+// console.log(person['languages'])
+// const key = 'hasGirlfriend'
+// person.hasGirlfriend = true
+// console.log(person[key])
